fc44f1f8e689dfa1669d899ecea162c1dbded9cf
Kristoffer Dalby <kristoffer@dalby.cc>
2026-06-13 19:55:39 +0200

    ci: run checks via kradalby/flake-checks

--- flake.nix
+++ flake.nix
@@ -4,24 +4,52 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-checks.url = "github:kradalby/flake-checks";
+    flake-checks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+  outputs = { self, nixpkgs, flake-utils, flake-checks }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        fc = flake-checks.lib;
+        common = {
+          inherit pkgs;
+          root = ./.;
+          pname = "gopkcs11";
+          version = "0.0.1";
+          vendorHash = "sha256-/Xka8rgNn04wSUG1x1K/uM7drMr5baWR1EfSYiqw0rc=";
+          goPkg = pkgs.go_1_26;
+        };
+
         softhsm2-lib = "${pkgs.softhsm}/lib/softhsm/libsofthsm2.so";
-        tpm2-pkcs11-lib = "${pkgs.tpm2-pkcs11}/lib/libtpm2_pkcs11.so";
+        tpm2-pkcs11-lib =
+          if pkgs.stdenv.isLinux
+          then "${pkgs.tpm2-pkcs11}/lib/libtpm2_pkcs11.so"
+          else "";
       in
       {
+        packages.default = fc.goBuild common;
+
+        formatter = fc.formatter common;
+
+        checks = {
+          build = fc.goBuild common;
+          gotest = fc.goTest common;
+          golangci-lint = fc.goLint common;
+          formatting = fc.goFormat common;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.go
             pkgs.softhsm
             pkgs.golangci-lint
             pkgs.opensc
-
-            # TPM2 PKCS#11 support (for //go:build tpm2 integration tests)
+          ]
+          # TPM2 PKCS#11 support (for //go:build tpm2 integration tests) is
+          # Linux-only; keep the dev shell evaluable on Darwin.
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
             pkgs.tpm2-pkcs11
             pkgs.tpm2-pkcs11.bin
             pkgs.tpm2-tools
